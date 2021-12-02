bae12d2351e380913d49003c582358f274d6d1ea
Jesse <dynaptx@163.com>
2021-12-02 21:39:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,19 @@ div1.className = "demo";
 
 document.body.appendChild(div1);
 
-document.body.onclick = function (e) {
-  console.log(e.clientX, e.clientY);
-  // console.log(e);
-  div1.style.left = e.clientX + "px";
-  div1.style.top = e.clientY + "px";
+let dragging = false;
+
+div1.onmousedown = function () {
+  dragging = true;
+};
+
+document.body.onmousemove = function (e) {
+  if (dragging === true) {
+    div1.style.left = e.clientX + "px";
+    div1.style.top = e.clientY + "px";
+  }
+};
+
+div1.onmouseup = function () {
+  dragging = false;
 };
