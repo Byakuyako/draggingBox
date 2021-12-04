c050faa1ac6d4ee0b46ff15ddbe524b77193b341
Jesse <dynaptx@163.com>
2021-12-04 21:17:58 +0800

    根据光标拖动的距离来同步移动 box

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 let div1 = document.createElement("div");
+
 div1.className = "demo";
 
 document.body.appendChild(div1);
 
 let dragging = false;
 
-div1.onmousedown = function () {
+let lastX;
+let lastY;
+
+div1.onmousedown = function (e) {
   dragging = true;
+  lastX = e.clientX;
+  lastY = e.clientY;
 };
 
 document.body.onmousemove = function (e) {
   if (dragging === true) {
-    div1.style.left = e.clientX + "px";
-    div1.style.top = e.clientY + "px";
+    let top = parseInt(div1.style.top) || 0;
+    let left = parseInt(div1.style.left) || 0;
+    let deltaX = e.clientX - lastX;
+    let deltaY = e.clientY - lastY;
+    div1.style.left = left + deltaX + "px";
+    div1.style.top = top + deltaY + "px";
+    lastX = e.clientX;
+    lastY = e.clientY;
   }
 };
 
